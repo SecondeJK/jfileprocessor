b044b1b048a145076c02fcafa0979bb108290dd6
Jim Seconde <jim.seconde@campaignamp.com>
2017-12-20 00:24:45 +0000

    took a long time to see autowiring, but got there. injected

--- src/Command/ParameterArrayCommand.php
+++ src/Command/ParameterArrayCommand.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Command;
 
+use App\Helper\ParameterHelper;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -8,6 +9,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ParameterArrayCommand extends ContainerAwareCommand
 {
+    public $parameterHelper;
+
     protected function configure()
     {
         $this
@@ -16,15 +19,27 @@ class ParameterArrayCommand extends ContainerAwareCommand
         ;
     }
 
+    public function __construct(ParameterHelper $parameterHelper)
+    {
+        $this->parameterHelper = $parameterHelper;
+        parent::__construct();
+    }
+
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Activated new command');
+        $output->writeln('Activated new command, we are wired');
 
         // test to make sure we're wired correctly
         $param1 = $this->getContainer()->getParameter('app.siteOne');
-        $output->writeln($param1);
+        $output->writeln("First parameter fetched from service container: {$param1}");
+        $output->writeln("Now attempting to get helper array of the stack...");
+        dump($this->parameterHelper->getParameterBag());
+        $output->writeln('I have a parameter, it is p3 j4 m2 4k and I need to know what service it is');
 
         // test to make sure our service is wired correctly
-        $helperService = $this->getContainer()->get('app.parameterHelper');
+        // $helperService = $this->getContainer()->get('app.parameterHelper');
+        // that did not work really as symfony is now more picky about dependency injection
+        // attempting to inject it instead with autowire
     }
 }
